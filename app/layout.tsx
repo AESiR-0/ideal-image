668f1e824e3ce7d7dc03745b3d186c0ff46c7191
Prat <90002355+AESiR-0@users.noreply.github.com>
2024-11-21 08:11:31 +0000

    Enhance layout: add Google Analytics and RB2B scripts conditionally in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -28,18 +27,76 @@ const Begum = localFont({
   variable: "--font-begum",
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Ideal Image",
   description: "Best in the USA",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        if(process.env.NODE_ENV !== 'production'){" "}
+        {
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-L68SB695HB"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-L68SB695HB');
+            `,
+              }}
+            ></script>
+
+            {/* RB2B Script */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              !function () {
+                var reb2b = window.reb2b = window.reb2b || [];
+                if (reb2b.invoked) return;
+                reb2b.invoked = true;
+                reb2b.methods = ["identify", "collect"];
+                reb2b.factory = function (method) {
+                  return function () {
+                    var args = Array.prototype.slice.call(arguments);
+                    args.unshift(method);
+                    reb2b.push(args);
+                    return reb2b;
+                  };
+                };
+                for (var i = 0; i < reb2b.methods.length; i++) {
+                  var key = reb2b.methods[i];
+                  reb2b[key] = reb2b.factory(key);
+                }
+                reb2b.load = function (key) {
+                  var script = document.createElement("script");
+                  script.type = "text/javascript";
+                  script.async = true;
+                  script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";
+                  var first = document.getElementsByTagName("script")[0];
+                  first.parentNode.insertBefore(script, first);
+                };
+                reb2b.SNIPPET_VERSION = "1.0.1";
+                reb2b.load("VN080H4YME6J");
+              }();
+            `,
+              }}
+            ></script>
+          </>
+        }
+      </head>
       <body
         className={`${Halenoir.variable} font-Halenoir ${Begum.variable} overflow-x-hidden w-full antialiased`}
       >
