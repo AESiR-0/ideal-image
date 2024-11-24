d011d4716c1ce9e5d5bc0ebe3f7de69ded1a8537
AESiR <cyborgkiller1008@gmail.com>
2024-11-24 16:03:56 +0530

    Enhance layout: add ReB2B and Google Analytics event snippets for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import logo from "@/public/static/symbol/32_32.png";
+import Script from "next/script";
 
 const Halenoir = localFont({
   src: [
@@ -42,6 +43,63 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {" "}
+        {/* ReB2B Script */}
+        <Script id="reb2b-script" strategy="afterInteractive">
+          {`!function () {
+          var reb2b = window.reb2b = window.reb2b || [];
+          if (reb2b.invoked) return;
+          reb2b.invoked = true;
+          reb2b.methods = ["identify", "collect"];
+          reb2b.factory = function (method) {
+            return function () {
+              var args = Array.prototype.slice.call(arguments);
+              args.unshift(method);
+              reb2b.push(args);
+              return reb2b;
+            };
+          };
+          for (var i = 0; i < reb2b.methods.length; i++) {
+            var key = reb2b.methods[i];
+            reb2b[key] = reb2b.factory(key);
+          }
+          reb2b.load = function (key) {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.async = true;
+            script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";
+            var first = document.getElementsByTagName("script")[0];
+            first.parentNode.insertBefore(script, first);
+          };
+          reb2b.SNIPPET_VERSION = "1.0.1";
+          reb2b.load("VN080H4YME6J");
+        }();`}
+        </Script>
+        {/* Event Snippet for Submit Lead Form */}
+        <Script id="gtag-conversion-snippet-1" strategy="afterInteractive">
+          {`gtag('event', 'conversion', {
+          'send_to': 'AW-16711899664/oaL1CNaSpNUZEJC07aA-',
+          'value': 1.0,
+          'currency': 'USD'
+        });`}
+        </Script>
+        {/* Event Snippet for gtag_report_conversion */}
+        <Script id="gtag-conversion-snippet-2" strategy="afterInteractive">
+          {`function gtag_report_conversion(url) {
+          var callback = function () {
+            if (typeof(url) != 'undefined') {
+              window.location = url;
+            }
+          };
+          gtag('event', 'conversion', {
+            'send_to': 'AW-16711899664/oaL1CNaSpNUZEJC07aA-',
+            'value': 1.0,
+            'currency': 'USD',
+            'event_callback': callback
+          });
+          return false;
+        }`}
+        </Script>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-L68SB695HB"
@@ -56,7 +114,6 @@ export default function RootLayout({
             `,
           }}
         ></script>
-
         {/* RB2B Script */}
         <script
           dangerouslySetInnerHTML={{
