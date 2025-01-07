d475315e3c2f03e6b3d60cde7628c1fbb007f0a7
AESiR <cyborgkiller1008@gmail.com>
2025-01-07 20:49:19 +0530

    Update HomeForm component headers for New Year promotion

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -125,7 +125,7 @@ const HomeForm = () => {
       <div className="h-[30vh] max-md:h-screen max-md:bg-transparent bg-[#0A5346] flex max-md:flex-col items-center md:justify-between relative">
         {/* Full-width Tis the Season Header */}
         <h3 className="absolute max-md:w-screen max-md:static top-0 left-0 w-full uppercase text-lg md:text-4xl font-extrabold tracking-wide text-[#0A5346] bg-white max-md:text-center text-left md:pl-[3rem] py-2">
-          'Tis the Holiday <span className="text-[#0A5346]">Season</span>
+          NEW YEAR, NEW YOU!
         </h3>
 
         {/* Left Side: Tis the Season Text */}
--- app/components/sections/HomeForm.tsx
+++ app/components/sections/HomeForm.tsx
@@ -130,7 +130,7 @@ const HomeForm = ({ page }: { page: string }) => {
             <div className="h-[30vh] max-md:h-screen max-md:bg-transparent bg-[#0A5346] flex max-md:flex-col items-center md:justify-between relative">
                 {/* Full-width Tis the Season Header */}
                 <h3 className="absolute max-md:w-screen max-md:static top-0 left-0 w-full uppercase text-lg md:text-4xl font-extrabold tracking-wide text-[#0A5346] bg-[#f6f4f2] max-md:text-center text-left md:pl-[3rem] py-2">
-                    'Tis the Holiday <span className="text-[#0A5346]">Season</span>
+                    Up to 40% off
                 </h3>
 
                 {/* Left Side: Tis the Season Text */}
