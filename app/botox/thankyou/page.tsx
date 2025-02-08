fcf3cefa65bcbbcb276fc636c5dc0f41f02160ee
Keyur Rawal <keyurrawal07@gmail.com>
2025-02-08 16:43:53 +0530

    Botox New

--- app/botox/thankyou/page.tsx
+++ app/botox/thankyou/page.tsx
@@ -13,11 +13,11 @@ const ThankYouPage = () => {
           What's Next?
         </h1>
         <p className="text-lg text-[#515b7e] mb-6">
-          You're just one step away from your CoolSculpting® transformation!
+          You're just one step away from your Botox transformation!
         </p>
 
         <p className="text-lg text-[#515b7e] font-medium mb-4">
-          Our CoolSculpting® specialists will be reaching out to you shortly. If
+          Our Botox specialists will be reaching out to you shortly. If
           you miss our call, feel free to text or email us back.
         </p>
 
@@ -26,21 +26,20 @@ const ThankYouPage = () => {
             Limited Time Offer
           </h2>
           <p className="text-lg">
-            Get upto <strong>47% off</strong> on your CoolSculpting® treatment
-            by scheduling your consultation today!
+            Get your Botox treatment for <strong>$9 per unit</strong> by scheduling your consultation today!
           </p>
         </div>
 
         <div className="text-left space-y-4">
           <h3 className="text-2xl font-bold font-Begum text-[#515b7e] mb-4">
-            Your CoolSculpting® Consultation Experience:
+            Your Botox Consultation Experience:
           </h3>
           <div className="flex items-start space-x-4">
             <span className="text-[#0A5346] font-bold">✔</span>
             <p className="text-[#515b7e]">
               <strong>Personalized Assessment:</strong> Our experts will
               evaluate your areas of concern and create a customized
-              CoolSculpting® treatment plan.
+              Botox treatment plan.
             </p>
           </div>
           <div className="flex items-start space-x-4">
@@ -54,15 +53,14 @@ const ThankYouPage = () => {
             <span className="text-[#0A5346] font-bold">✔</span>
             <p className="text-[#515b7e]">
               <strong>Immediate Treatment Option:</strong> If you're ready,
-              start your CoolSculpting® journey the same day as your
+              start your Botox journey the same day as your
               consultation.
             </p>
           </div>
         </div>
 
         <p className="text-lg text-[#515b7e] mt-6">
-          You're about to join thousands of satisfied clients who have achieved
-          their body contouring goals with CoolSculpting® at Ideal Image!
+          You're about to join thousands of satisfied clients who have refreshed and rejuvenated their appearance with Botox at Ideal Image!
         </p>
       </div>
 
